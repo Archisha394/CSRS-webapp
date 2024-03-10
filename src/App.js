c6f0295b168228b6db3031e84f316305d2848f1d
Archisha394 <guptaarchisha394@kgpian.iitkgp.ac.in>
2024-03-10 11:50:31 +0530

    sidebar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar/Navbar";
+import React from "react";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <div className="main-content">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
